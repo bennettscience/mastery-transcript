d0293fc0d0c8c2eb1e33d872faaaaf6df9aec373
bennettscience <brian.bennett2@gmail.com>
2020-12-25 20:58:22 -0500

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Flask, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_debugtoolbar import DebugToolbarExtension
+from config import Config
+
+__version__ = '0.1'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes
+from models.user import User
